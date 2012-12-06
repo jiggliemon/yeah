eeafb1757c4800b317fc2d57013bc2f5f9d792ad
Chase Wilson <chase@agroism.com>
2012-12-06 00:01:04 -0800

    Added build profiled for require.js && some compiled versions for AMD consumption

---
+++ build/build.js
@@ -0,0 +1,15 @@
+({
+   baseUrl: "../"
+  ,name: 'yeah'
+  ,packages:[
+    {name: 'yeah', location: 'src',main: 'index'}
+  ]
+  // ,paths: {
+  //     "yeah":"index"
+  //    ,"yeah/mixin":"mixin"
+  // }
+  ,optimize: 'none'
+  //,findNestedDependencies: true
+  ,out: "../yeah.js"
+  ,cjsTranslate: true
+})
